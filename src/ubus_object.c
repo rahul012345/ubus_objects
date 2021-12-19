7f207facf941ce182873b98d4177645af85887d6
rahul <er_rahul@live.in>
2021-12-19 18:02:29 +0530

    Adding notification code testing

--- src/ubus_object.c
+++ src/ubus_object.c
@@ -32,6 +32,54 @@ static const int log_class[] = {
 	[L_INFO] = LOG_INFO,
 	[L_DEBUG] = LOG_DEBUG};
 
+//Add notification
+static struct ubus_event_handler interface_event_handler;
+
+enum {
+	EVT_INTF,
+	EVT_ACTN,
+};
+
+static const struct blobmsg_policy event_policy[] = {
+	[EVT_INTF] = {.name = "interface", .type = BLOBMSG_TYPE_STRING},
+	[EVT_ACTN] = {.name = "action", .type = BLOBMSG_TYPE_STRING},
+};
+
+static void ubus_receive_interface_event(
+	__attribute__((unused)) struct ubus_context* ctx,
+	__attribute__((unused)) struct ubus_event_handler* ev,
+	__attribute__((unused)) const char* type, struct blob_attr* msg)
+{
+	const char* interface = NULL;
+	const char* action = NULL;
+	struct blob_attr* tb[ARRAY_SIZE(event_policy)];
+
+	blobmsg_parse(event_policy, ARRAY_SIZE(event_policy), tb, blob_data(msg),
+				  blob_len(msg));
+
+	if (tb[EVT_INTF] && tb[EVT_ACTN]) {
+		interface = blobmsg_get_string(tb[EVT_INTF]);
+		action = blobmsg_get_string(tb[EVT_ACTN]);
+		notify_interface(interface, action);
+	}
+}
+
+bool ubus_listen_network_events(void)
+{
+	/* ubus event listener */
+	memset(&interface_event_handler, 0, sizeof(interface_event_handler));
+	interface_event_handler.cb = ubus_receive_interface_event;
+	int ret = ubus_register_event_handler(ctx, &interface_event_handler,
+										  "network.interface");
+	if (ret < 0) {
+		return false;
+	}
+
+	ubus_add_uloop(ctx);
+	return true;
+}
+//End notification
+
 static int ubus_rnd_handler(struct ubus_context *ctx, struct ubus_object *obj,
 							struct ubus_request_data *req,
 							const char *method, struct blob_attr *msg);
@@ -104,6 +152,7 @@ int main()
 	my_log_message(log_class[LOG_DEBUG], "UBUS_ID Hello world");
 	struct uci_package *my_config_pkg = NULL;
 	struct uci_context *context = uci_alloc_context();
+
 	my_config_pkg = calloc(1,sizeof(struct uci_package));
 
 	if(uci_load(context , "/etc/config/alm",&my_config_pkg)){
@@ -129,7 +178,7 @@ int main()
 				}
 
 
-			}
+				}
 		}
 
 	}
